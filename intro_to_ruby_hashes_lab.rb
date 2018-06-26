58f4653ef926ff3eb7ad7f2f16b6d5447b468f04
Priscilla H <priscillamhuynh@gmail.com>
2018-06-26 11:03:15 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,7 +27,7 @@ monopoly={:railroads=>
 							:one_piece_owned=>25,
 							:two_pieces_owned=>50,
 							:three_pieces_owned=>100,
-							:four_pieces_owned=>200}}}
+							:four_pieces_owned=>200}}
 end
 
 def monopoly_with_fourth_tier
