ae74f617f45cfb522ea0a048ddcf70377c143eaa
Priscilla H <priscillamhuynh@gmail.com>
2018-06-26 11:04:04 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,7 +21,7 @@ monopoly={:railroads=>
 							:reading_railroad=>{},
 							:pennsylvania_railroad=>{},
 							:b_and_o_railroad=>{},
-							:shortline{}
+							:shortline=>{}
 						},
 						:rent_in_dollars=>{
 							:one_piece_owned=>25,
